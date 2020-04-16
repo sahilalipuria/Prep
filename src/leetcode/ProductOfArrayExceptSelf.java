4378182724c73b9e608cb4c1cc486f87acbb5aa7
Sahil Alipuria <salipuri@cisco.com>
2020-04-16 11:22:14 +0530

    Product of Array except Self

---
+++ src/leetcode/ProductOfArrayExceptSelf.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package leetcode;
+
+import java.util.HashSet;
+
+/**
+ * @author salipuri
+ * 
+ * Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+ *
+ */
+public class ProductOfArrayExceptSelf {
+
+	/**
+	 * @param args
+	 */
+	
+	public static int[] productExceptSelf(int[] nums) {
+       
+		int[] leftSum = new int[nums.length];
+		int[] rightSum = new int[nums.length];
+		
+		leftSum[0] = 1;
+		rightSum[nums.length-1] = 1;
+		
+		for(int i=1;i<nums.length;i++) {
+			leftSum[i] = leftSum[i-1]*nums[i-1];
+		}
+		
+		for(int i=nums.length-2;i>=0;i--) {
+			rightSum[i] = rightSum[i+1]*nums[i+1];
+		}
+		
+		for(int i=0;i<nums.length;i++) {
+			nums[i] = leftSum[i]*rightSum[i];
+			System.out.print(nums[i]+",");
+		}
+		
+        return nums;
+    }
+	
+	public static void main(String[] args) {
+		
+		productExceptSelf(new int[] {1,2,3,4});
+
+	}
+
+}
