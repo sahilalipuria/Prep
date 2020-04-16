9a914b901540d653e5403e3ebe13f3c17cdd2380
Sahil Alipuria <salipuri@cisco.com>
2020-04-16 11:41:06 +0530

    Product of Array except Self modified variable

--- src/leetcode/ProductOfArrayExceptSelf.java
+++ src/leetcode/ProductOfArrayExceptSelf.java
@@ -30,22 +30,22 @@ public class ProductOfArrayExceptSelf {
 	
 	public static int[] productExceptSelf(int[] nums) {
        
-		int[] leftSum = new int[nums.length];
-		int[] rightSum = new int[nums.length];
+		int[] leftProduct = new int[nums.length];
+		int[] rightProduct = new int[nums.length];
 		
-		leftSum[0] = 1;
-		rightSum[nums.length-1] = 1;
+		leftProduct[0] = 1;
+		rightProduct[nums.length-1] = 1;
 		
 		for(int i=1;i<nums.length;i++) {
-			leftSum[i] = leftSum[i-1]*nums[i-1];
+			leftProduct[i] = leftProduct[i-1]*nums[i-1];
 		}
 		
 		for(int i=nums.length-2;i>=0;i--) {
-			rightSum[i] = rightSum[i+1]*nums[i+1];
+			rightProduct[i] = rightProduct[i+1]*nums[i+1];
 		}
 		
 		for(int i=0;i<nums.length;i++) {
-			nums[i] = leftSum[i]*rightSum[i];
+			nums[i] = leftProduct[i]*rightProduct[i];
 			System.out.print(nums[i]+",");
 		}
 		
