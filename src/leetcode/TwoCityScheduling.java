eaa54e192ad61bed3917bb80e34c86eb4587f544
Sahil Alipuria <salipuri@cisco.com>
2020-06-03 13:43:35 +0530

    Two City Scheduling

---
+++ src/leetcode/TwoCityScheduling.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author salipuri
+ * 
+ *         There are 2N people a company is planning to interview. The cost of
+ *         flying the i-th person to city A is costs[i][0], and the cost of
+ *         flying the i-th person to city B is costs[i][1].
+ * 
+ *         Return the minimum cost to fly every person to a city such that
+ *         exactly N people arrive in each city.
+ * 
+ * 
+ * 
+ *         Example 1:
+ * 
+ *         Input: [[10,20],[30,200],[400,50],[30,20]] Output: 110 
+ *         Explanation:
+ *         The first person goes to city A for a cost of 10. 
+ *         The second person goes to city A for a cost of 30. 
+ *         The third person goes to city B for a cost of 50. 
+ *         The fourth person goes to city B for a cost of 20.
+ * 
+ *         The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the
+ *         people interviewing in each city.
+ * 
+ * 
+ *         Note:
+ * 
+ *         1 <= costs.length <= 100 It is guaranteed that costs.length is even.
+ *         1 <= costs[i][0], costs[i][1] <= 1000
+ *
+ */
+public class TwoCityScheduling {
+
+	/**
+	 * @param args
+	 */
+	
+
+	public static int twoCitySchedCost(int[][] costs) {
+
+		Arrays.sort(costs, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return (a[1] - a[0]) - (b[1] - b[0]);
+            }
+        });
+		
+        int cost = 0;
+        for (int i = 0; i < costs.length / 2; i++) {
+            cost += costs[i][1] + costs[costs.length-i-1][0];
+        }
+        return cost;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(twoCitySchedCost(new int[][] {{259,770},{448,54},{926,667},{184,139},{840,118},{577,469}}));
+	}
+
+}
